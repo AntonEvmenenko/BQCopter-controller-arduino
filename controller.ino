a177d033739ea0eec70d72c01138c576ad2cd2f8
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2016-01-29 23:15:53 +0300

    control by camera added

--- controller.ino
+++ controller.ino
@@ -9,8 +9,10 @@ void setup( ) {
     Mirf.init( );  
     Mirf.setRADDR( ( byte* )"serv1" );
     Mirf.channel = 90;
-    Mirf.payload = sizeof( byte ) * 3;
+    Mirf.payload = sizeof( byte ) * 4;
     Mirf.config( );
+
+    pinMode(5, INPUT);
 }
 
 void loop() {
@@ -18,6 +20,7 @@ void loop() {
     data[ 0 ] = 255 - ( byte )( analogRead( A4 ) / 4 ); // throttle
     data[ 1 ] = 255 - ( byte )( analogRead( A1 ) / 4 ); // roll
     data[ 2 ] = ( byte )( analogRead( A3 ) / 4 ); // pitch
+    data[ 3 ] = (digitalRead(5) == HIGH) ? 0 : 1;
     
     //Serial.println(data);
     Mirf.setTADDR( ( byte* )"serv2" );
